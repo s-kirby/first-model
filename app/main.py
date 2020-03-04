acc8a2880f6b046ee2140a9f58c64aa16a2af1f9
s-kirby <me@skirby.dev>
2020-03-04 10:17:25 -0600

    Had tensorflow gpu installed when needed tensorflow-cpu, fixed unclearable errors, started documentation.

--- app/main.py
+++ app/main.py
@@ -4,18 +4,18 @@ import tensorflow as tf
 
 
 
-print()
+print() # used to seperate the unclearable errors
 print("Tensorflow Version:")
 print(tf.__version__)
 print()
 
-mnist = tf.keras.datasets.mnist
+mnist = tf.keras.datasets.mnist # pulling installed mnist dataset
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
+model = tf.keras.models.Sequential([ # Defining our model
+  tf.keras.layers.Flatten(input_shape=(28, 28)), 
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(10)
