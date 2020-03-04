e444f430d5d21d60942228b7dd86fde3c1e3d437
s-kirby <me@skirby.dev>
2020-03-04 10:01:12 -0600

    Fixed imports, added .env file and path variables to settings.json

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import math
+import tensorflow as tf
+
+
+
+print()
+print("Tensorflow Version:")
+print(tf.__version__)
+print()
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
+
